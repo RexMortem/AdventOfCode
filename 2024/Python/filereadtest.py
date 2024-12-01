d24b5ae1dede08ec1de6c15fcf0575488f6eae7f
RexMortem <68436000+RexMortem@users.noreply.github.com>
2024-12-01 23:14:00 +0000

    File read perf test

--- 2024/Python/filereadtest.py
+++ 2024/Python/filereadtest.py
@@ -5,9 +5,25 @@ qNo = filePath[-1][:-3] # taking advantage of the fact you can index backwards i
 
 pathToInput = __file__[:-1 * (len(filePath[-1])+len(filePath[-2]) + 1)] + "Input\\" + qNo + ".in" # (the +1 is because of the '\' in Python\blueprint.py)
 
-# Method 1 
+# Methods 
 
-lines = None 
+def method1():
+    lines = None 
 
-with open(pathToInput,"r") as file:
-    lines = file.read().strip().split("\n")
+    with open(pathToInput,"r") as file:
+        lines = file.read().strip().split("\n")
+
+def method2():
+    lines = open(pathToInput,"r").read().strip().split("\n")
+
+def method3():
+    lines = open(pathToInput,"r").readlines()
+
+methods = [method1, method2, method3]
+
+# Let's time them
+
+for i in range(len(methods)):
+    print(f"Method {i+1}: {timeit.timeit(stmt=methods[i], number=500)}")
+
+# inconsistent BUT Method 1 is generally fastest
