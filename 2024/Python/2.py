a182266960416846816ef6b6059a05bfcf7082de
RexMortem <68436000+RexMortem@users.noreply.github.com>
2024-12-02 15:43:10 +0000

    2024 Day 2

--- 2024/Python/2.py
+++ 2024/Python/2.py
@@ -13,4 +13,147 @@ lines = None
 with open(pathToInput,"r") as file:
     lines = file.read().strip().split("\n")
 
-print(lines)
+safeCount = 0
+
+for report in lines: 
+    values = map(int, report.split())
+    prevValue = None 
+    increasing = None 
+    reportVal = True 
+
+    for value in values:
+        if not prevValue:
+            prevValue = value 
+            continue 
+        
+        if increasing is None:
+            if value > prevValue:
+                increasing = True 
+            else:
+                increasing = False 
+        else:
+            if (increasing and (value < prevValue)):
+                reportVal = False 
+                break
+            elif ((not increasing) and (value > prevValue)):
+                reportVal = False 
+                break
+
+        # check other safe cond
+
+        if reportVal and not (1 <= abs(value - prevValue) <= 3):
+            reportVal = False 
+        
+        if not reportVal:
+            break
+    
+        prevValue = value 
+    
+    if reportVal:
+        safeCount += 1 
+        
+print(safeCount)
+
+# Part 2 
+
+def stdCheckReport(values):
+    prevValue = None 
+    increasing = None 
+    reportVal = True 
+
+    for value in values:
+        if not prevValue:
+            prevValue = value 
+            continue 
+        
+        if increasing is None:
+            if value > prevValue:
+                increasing = True 
+            else:
+                increasing = False 
+        else:
+            if (increasing and (value < prevValue)):
+                reportVal = False 
+                break
+            elif ((not increasing) and (value > prevValue)):
+                reportVal = False 
+                break
+
+        # check other safe cond
+
+        if reportVal and not (1 <= abs(value - prevValue) <= 3):
+            reportVal = False 
+        
+        if not reportVal:
+            break
+    
+        prevValue = value 
+    
+    return reportVal 
+
+oSafeCount = 0
+
+for report in lines: 
+    values = list(map(int, report.split()))
+    prevValue = None 
+    increasing = None 
+    reportVal = True 
+
+    for i in range(len(values)):
+        value = values[i]
+
+        if not prevValue:
+            prevValue = value 
+            continue 
+        
+        if increasing is None:
+            if value > prevValue:
+                increasing = True 
+            else:
+                increasing = False 
+        else:
+            if (increasing and (value < prevValue)):
+                reportVal = False 
+            elif ((not increasing) and (value > prevValue)):
+                reportVal = False 
+
+        # check other safe cond
+
+        if reportVal and not (1 <= abs(value - prevValue) <= 3):
+            reportVal = False 
+        
+        
+        if not reportVal:
+            # let's see if we can save the report! Either remove value or prevValue or even prevPrevValue
+            A = values.copy()
+            A.pop(i)
+            reportVal = stdCheckReport(A)
+
+            if reportVal:
+                break
+
+            # let's try with removing prevValue instead...
+            B = values.copy()
+            B.pop(i-1)
+            reportVal = stdCheckReport(B)
+
+            if reportVal or (i <= 1):
+                break
+
+            # removing prevPrevValue
+            C = values.copy()
+            C.pop(i-2)
+            reportVal = stdCheckReport(C)
+
+            break
+
+        prevValue = value 
+
+    if reportVal:
+        #print("safe:", report)
+        oSafeCount += 1 
+    else:
+        #print("unsafe:", report)
+        pass
+        
+print(oSafeCount)
